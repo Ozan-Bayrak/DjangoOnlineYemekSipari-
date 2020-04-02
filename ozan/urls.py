1da0f60c69834302ce44bb7e7a470e9dc14990ad
Ozan-Bayrak <bayrakozan4@gmail.com>
2020-04-02 17:29:24 +0300

    About Us Ve Contact sayfaları yapımı

--- ozan/urls.py
+++ ozan/urls.py
@@ -18,8 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from home import views
+
 urlpatterns = [
     path('', include('home.urls')),
+    path('aboutus/', views.aboutus, name='aboutus'),
+    path('contact/', views.contact, name='contact'),
     path('restaurant/', include('restaurant.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
