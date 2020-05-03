5b78bad07af52ad0a751d9f8bd4408cb66dc10dc
Ozan-Bayrak <bayrakozan4@gmail.com>
2020-05-03 18:20:53 +0300

    Sipariş yapma ve Tamamlama

--- order/admin.py
+++ order/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from order.models import ShopCart
+from order.models import ShopCart, OrderProduct, Order
 
 
 class ShopCartAdmin(admin.ModelAdmin):
     list_display = ['user','restaurant','food','price','quantity','amount']
     list_filter = ['user']
 
+class OrderProductline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('user','restaurant','foods','price','quantity','amount')
+    can_delete = False
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','phone','total','status']
+    list_filter = ['status']
+    readonly_fields = ('user','address','phone','first_name','ip','total')
+    inlines = [OrderProductline]
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['user','foods','restaurant','price','quantity','amount']
+    list_filter = ['user']
+
 admin.site.register(ShopCart,ShopCartAdmin)
+admin.site.register(Order,OrderAdmin)
+admin.site.register(OrderProduct,OrderProductAdmin)
--- order/views.py
+++ order/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from order.models import ShopCartForm, ShopCart
-from restaurant.models import Category
+from django.utils.crypto import get_random_string
+
+from home.models import UserProfile
+from order.models import ShopCartForm, ShopCart, OrderForm, Order, OrderProduct
+from restaurant.models import Category, Foods
 
 
 def index(request):
@@ -36,7 +39,6 @@ def addtocart(request,id):
                 data.quantity = form.cleaned_data['quantity']
                 data.save() # DB kaydet
             request.session['cart_items']=ShopCart.objects.filter(user_id=current_user.id).count() #count item in shopcart
-            messages.success(request,'Ürün Sepete Eklendi.')
 
             return HttpResponseRedirect(url)
 
@@ -65,3 +67,67 @@ def deletefromcart(request,id):
     request.session['cart_items'] = ShopCart.objects.filter(user_id=current_user.id).count()  # count item in shopcart
     messages.success(request,"Ürün Sepetten Silinmiştir.")
     return HttpResponseRedirect("/shopcart")
+
+
+@login_required(login_url='/login') #check login
+def orderproduct(request):
+    category = Category.objects.all()
+    current_user = request.user
+    scart = ShopCart.objects.filter(user_id=current_user.id)
+    total = 0
+    for rs in scart:
+        total += rs.food.price * rs.quantity
+
+    if request.method == 'POST': #if there is a post
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            data =Order()
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.address = form.cleaned_data['address']
+            data.phone = form.cleaned_data['phone']
+            data.user_id = current_user.id
+            data.total = total
+            data.ip = request.META.get('REMOTE_ADDR')
+            ordercode = get_random_string(5).upper()
+            data.code = ordercode
+            data.save()
+
+            #Move shopcart items to order products items
+            scart = ShopCart.objects.filter(user_id=current_user.id)
+            for rs in scart:
+                detail = OrderProduct()
+                detail.order_id = data.id
+                detail.restaurant_id = rs.food.restaurant_id
+                detail.foods_id = rs.food_id
+                detail.user_id = current_user.id
+                detail.quantity = rs.quantity
+                detail.price  = rs.food.price
+                detail.amount = rs.amount
+                detail.save()
+
+            ShopCart.objects.filter(user_id=current_user.id).delete() #clear shopcart
+            request.session['cart_items']=0
+            return render(request,'Order_Completed.html',{'ordercode':ordercode,'category':category})
+
+        else:
+            return HttpResponseRedirect("/order/orderproduct")
+
+    form = OrderForm()
+    profile = UserProfile.objects.get(user_id=current_user.id)
+    context = {
+        'scart':scart,
+        'category':category,
+        'total':total,
+        'form':form,
+        'profile':profile
+    }
+    return render(request,'Order_Form.html',context)
+
+
+
+
+
+
+
